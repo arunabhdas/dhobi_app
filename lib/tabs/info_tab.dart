ec9f05f0ee8db89437a41505552df8c61ab4ed01
Aadarsha Bastola <aadarsha.com@gmail.com>
2021-03-09 00:27:29 -0700

    infoTab

--- lib/tabs/info_tab.dart
+++ lib/tabs/info_tab.dart
@@ -4,15 +4,31 @@ class InfoTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          shadowColor: Colors.black45,
-          backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 1,
+        shadowColor: Colors.black45,
+        backgroundColor: Colors.white,
+        title: Text(
+          'The Dhobi Service',
+          style: TextStyle(fontSize: 20, color: Colors.purple[900]),
+        ),
+      ),
+      body: Container(
+        child: ListTile(
+          leading: Icon(
+            Icons.attach_money_sharp,
+            size: 20,
+            color: Colors.purple[900],
+          ),
           title: Text(
-            'About Us',
-            style: TextStyle(fontSize: 20, color: Colors.purple[900]),
+            'Name',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.purple[900],
+            ),
           ),
         ),
-        body: Container());
+      ),
+    );
   }
 }
